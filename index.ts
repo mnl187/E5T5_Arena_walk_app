d236ae186dd62ecd3ea0292ef63a822c08140466
mnl187 <mnl187@gmail.com>
2022-11-22 17:32:53 +0100

    dodanie hali sław

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import {engine} from "express-handlebars";
 import {homeRouter} from "./routers/home";
 import {warriorRouter} from "./routers/warrior";
 import {arenaRouter} from "./routers/arena";
+import {hallOfFameRouter} from "./routers/hall-of-fame";
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.set('view engine', '.hbs');
 app.use('/', homeRouter);
 app.use('/warrior', warriorRouter);
 app.use('/arena', arenaRouter);
+app.use('/hall-of-fame', hallOfFameRouter);
 // app.use(handleError);
 
 app.listen(3000, 'localhost', () => {
