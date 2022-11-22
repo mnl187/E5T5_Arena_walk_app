5fbeefe4eae6d37dc84473c5366ae14eeda6369e
mnl187 <mnl187@gmail.com>
2022-11-22 06:54:16 +0100

    dodanie silnika handle-bars (FRONTEND)

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import methodOverride = require("method-override");
-import {urlencoded} from "express";
+import {static as eStatic, urlencoded} from "express";
+import {engine} from "express-handlebars";
 
 const app = express();
 
@@ -8,3 +9,8 @@ app.use(methodOverride('_method'));
 app.use(urlencoded({
     extended: true,
 }));
+app.use(eStatic('public'));
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    //helpers??
+}))
