d416f96fd4b102eb79b525cbd15a1f6929b38fc1
mnl187 <mnl187@gmail.com>
2022-11-22 17:14:03 +0100

    dodanie nasłuchiwania

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import 'express-async-errors';
 import methodOverride = require("method-override");
 import {static as eStatic, urlencoded} from "express";
 import {engine} from "express-handlebars";
@@ -19,3 +20,9 @@ app.set('view engine', '.hbs');
 app.get('/', (req, res) => {
     res.send('Hello!');
 });
+
+// app.use(handleError);
+
+app.listen(3000, 'localhost', () => {
+console.log('Listening on http://localhost:3000');
+})
