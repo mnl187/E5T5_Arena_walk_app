158d4adff4063512f2ec5b319dc41612d8c1053a
mnl187 <mnl187@gmail.com>
2022-11-28 06:55:15 +0100

    dodanie komunikatu błędu walidacji imienia

--- routers/warrior.ts
+++ routers/warrior.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/error";
 
 export const warriorRouter = Router();
 
@@ -9,6 +10,9 @@ warriorRouter
         res.render('warrior/add-form');
     })
     .post('/', async (req, res) => {
+        if (await WarriorRecord.isNameTaken(req.body.name)) {
+            throw new ValidationError(`To imię jest już zajęte. Wybierz inne.`);
+        }
         const warrior = new WarriorRecord({
             ...req.body,
             power: Number(req.body.power),
